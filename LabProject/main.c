07c7eb955cf6d74f4bf6e4543f1a14e692d002d7
Yeobi <yeobi1124@gmail.com>
2025-10-01 13:02:09 +0900

    PWM Lab

--- LabProject/main.c
+++ LabProject/main.c
@@ -6,6 +6,29 @@
 #define LED_GREEN   0b010
 #define LED_BLUE    0b100
 
+void systick_init()
+{
+    SysTick->LOAD = 0x00FFFFFF;
+    SysTick->CTRL = 0x00000005;
+}
+
+void systick_wait1ms()
+{
+    SysTick->LOAD = 48000; // 48000 MHz Clock Hardware?
+    SysTick->VAL = 0;
+    while((SysTick->CTRL & 0x00010000) == 0){};
+}
+
+void systick_wait1s()
+{
+    int i = 0;
+    int count = 1000;
+    for(i=0;i<count;i++)
+    {
+        systick_wait1ms();
+    }
+}
+
 void led_init()
 {
     P2->SEL0 &= ~0x07;
@@ -40,6 +63,24 @@ void IR_Init()
     P7->DIR  &= ~0xFF;      // INPUT
 }
 
+void pwm_init34(uint16_t period, uint16_t duty3, uint16_t duty4)
+{
+    TIMER_A0->CCR[0] = period;
+
+    TIMER_A0->EX0 =0x0000;
+
+    TIMER_A0->CCTL[3] = 0x0040;
+    TIMER_A0->CCR[3] = duty3;
+    TIMER_A0->CCTL[4] = 0x0040;
+    TIMER_A0->CCR[4] = duty4;
+
+    TIMER_A0->CTL = 0x02F0;
+
+    P2->DIR |= 0xC0;
+    P2->SEL0 |= 0xC0;
+    P2->SEL1 &=~0xC0;
+}
+
 void motor_init()
 {
     P3->SEL0 &= ~0xC0;
@@ -56,7 +97,38 @@ void motor_init()
     P2->SEL1 &= ~0xC0;
     P2->DIR  |=  0xC0;
     P2->OUT  &= ~0xC0;
+
+    pwm_init34(7500, 0, 0);
 }
+
+void move(uint16_t leftDuty, uint16_t rightDuty)
+{
+    P3->OUT |= 0xC0;
+    TIMER_A0->CCR[3] = leftDuty;
+    TIMER_A0->CCR[4] = rightDuty;
+}
+
+void left_forward()
+{
+    P5->OUT &= ~0x10;
+}
+
+void left_backward()
+{
+    P5->OUT |= 0x10;
+}
+
+void right_forward()
+{
+    P5->OUT &= ~0x20;
+}
+
+void right_backward()
+{
+    P5->OUT |= 0x20;
+}
+
+
 
 int main(void)
 {
@@ -65,6 +137,7 @@ int main(void)
     led_init();
     IR_Init();
     motor_init();
+    systick_init();
 
     int sensor;
     // Running
@@ -80,22 +153,33 @@ int main(void)
 
         P7->DIR = 0x00;
 
-        Clock_Delay1us(3000);
+        Clock_Delay1us(1000);
 
         if((P7->IN & 0b01111110) == 0b01111110){
             P2->OUT &= ~0x07;
             P2->OUT |=  0x02;
+
             P2->OUT &= ~0xC0;
+
+            move(0,0);
         }
         else if((P7->IN & 0b00011000) == 0b00011000){
+            P2->OUT &= ~0x07;
             P2->OUT |= 0x01;
-            P5->OUT &= ~0x30;
+
+//            P5->OUT &= ~0x30;
+            right_forward();
+            left_forward();
+            move(1500, 1500);
             P2->OUT |=  0xC0;
-            P3->OUT |=  0xC0;
+//            P3->OUT |=  0xC0;
+            systick_wait1ms();
         }
         else{
             P2->OUT &= ~0x07;
             P2->OUT &= ~0xC0;
+
+            move(0,0);
         }
 
         P5->OUT &= ~0x08;
