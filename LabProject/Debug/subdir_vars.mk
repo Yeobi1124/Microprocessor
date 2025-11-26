f7ea19d3bdc87c5fd3bf07a383503b31ac5aa277
Yeobi <yeobi1124@gmail.com>
2025-11-26 11:32:54 +0900

    feat : add stage 1 code

--- LabProject/Debug/subdir_vars.mk
+++ LabProject/Debug/subdir_vars.mk
@@ -11,36 +11,60 @@ CMD_SRCS += \
 C_SRCS += \
 ../Clock.c \
 ../main.c \
+../stage1.c \
+../stage2.c \
+../stage3.c \
+../stage4.c \
 ../startup_msp432p401r_ccs.c \
 ../system_msp432p401r.c 
 
 C_DEPS += \
 ./Clock.d \
 ./main.d \
+./stage1.d \
+./stage2.d \
+./stage3.d \
+./stage4.d \
 ./startup_msp432p401r_ccs.d \
 ./system_msp432p401r.d 
 
 OBJS += \
 ./Clock.obj \
 ./main.obj \
+./stage1.obj \
+./stage2.obj \
+./stage3.obj \
+./stage4.obj \
 ./startup_msp432p401r_ccs.obj \
 ./system_msp432p401r.obj 
 
 OBJS__QUOTED += \
 "Clock.obj" \
 "main.obj" \
+"stage1.obj" \
+"stage2.obj" \
+"stage3.obj" \
+"stage4.obj" \
 "startup_msp432p401r_ccs.obj" \
 "system_msp432p401r.obj" 
 
 C_DEPS__QUOTED += \
 "Clock.d" \
 "main.d" \
+"stage1.d" \
+"stage2.d" \
+"stage3.d" \
+"stage4.d" \
 "startup_msp432p401r_ccs.d" \
 "system_msp432p401r.d" 
 
 C_SRCS__QUOTED += \
 "../Clock.c" \
 "../main.c" \
+"../stage1.c" \
+"../stage2.c" \
+"../stage3.c" \
+"../stage4.c" \
 "../startup_msp432p401r_ccs.c" \
 "../system_msp432p401r.c" 
 
---
+++ LabProject/stage1.c
@@ -0,0 +1,318 @@
+#include "msp.h"
+#include "Clock.h"
+#include <stdio.h>
+
+#define LED_RED     0b001
+#define LED_GREEN   0b010
+#define LED_BLUE    0b100
+
+// systick
+void systick_init()
+{
+    SysTick->LOAD = 0x00FFFFFF;
+    SysTick->CTRL = 0x00000005;
+}
+
+void systick_wait1ms()
+{
+    SysTick->LOAD = 48000; // 48000 MHz Clock Hardware?
+    SysTick->VAL = 0;
+    while((SysTick->CTRL & 0x00010000) == 0){};
+}
+
+void systick_wait1s()
+{
+    int i = 0;
+    int count = 1000;
+    for(i=0;i<count;i++)
+    {
+        systick_wait1ms();
+    }
+}
+
+// led
+void led_init()
+{
+    P2->SEL0 &= ~0x07;
+    P2->SEL1 &= ~0x07;
+    P2->DIR |= 0x07;
+    P2->OUT &= ~0x07;
+}
+
+void turn_on_led(int n)
+{
+    P2->OUT  &= ~0x07;
+    P2->OUT |= n;
+}
+
+// IR Emitter
+void IR_Init()
+{
+    // 0, 2, 4, 6 IR Emitter
+    P5->SEL0 &= ~0x08;
+    P5->SEL1 &= ~0x08;      // GPIO
+    P5->DIR  |=  0x08;      // OUTPUT
+    P5->OUT  &= ~0x08;      // turn off 4 even IR LEDs
+
+    // 1, 3, 5, 7 IR Emitter
+    P9->SEL0 &= ~0x04;
+    P9->SEL1 &= ~0x04;      // GPIO
+    P9->DIR  |=  0x04;      // OUTPUT
+    P9->OUT  &= ~0x04;      // turn off 4 odd IR LEDs
+
+    // 0~7 IR SEnsor
+    P7->SEL0 &= ~0xFF;
+    P7->SEL1 &= ~0xFF;      // GPIO
+    P7->DIR  &= ~0xFF;      // INPUT
+}
+
+void pwm_init34(uint16_t period, uint16_t duty3, uint16_t duty4)
+{
+    TIMER_A0->CCR[0] = period;
+
+    TIMER_A0->EX0 =0x0000;
+
+    TIMER_A0->CCTL[3] = 0x0040;
+    TIMER_A0->CCR[3] = duty3;
+    TIMER_A0->CCTL[4] = 0x0040;
+    TIMER_A0->CCR[4] = duty4;
+
+    TIMER_A0->CTL = 0x02F0;
+
+    P2->DIR |= 0xC0;
+    P2->SEL0 |= 0xC0;
+    P2->SEL1 &=~0xC0;
+}
+
+// Timer A2
+void (*TimerA2Task) (void);
+void TimerA2_Init(void(*task)(void), uint16_t period)
+{
+    TimerA2Task = task;
+    TIMER_A2->CTL = 0x0280;
+    TIMER_A2->CCTL[0] = 0x0010;
+    TIMER_A2->CCR[0] = (period - 1);
+    TIMER_A2->EX0 = 0x0005;
+
+    NVIC->IP[3] = (NVIC->IP[3] & 0xFFFFFF00) | 0x00000040;
+    NVIC->ISER[0] = 0x00001000;
+    TIMER_A2->CTL |= 0x0014;
+}
+
+// motor
+void motor_init()
+{
+    P3->SEL0 &= ~0xC0;
+    P3->SEL1 &= ~0xC0;
+    P3->DIR  |=  0xC0;
+    P3->OUT  &= ~0xC0;
+
+    P5->SEL0 &= ~0x30;
+    P5->SEL1 &= ~0x30;
+    P5->DIR  |=  0x30;
+    P5->OUT  &= ~0x30;
+
+    P2->SEL0 &= ~0xC0;
+    P2->SEL1 &= ~0xC0;
+    P2->DIR  |=  0xC0;
+    P2->OUT  &= ~0xC0;
+
+    pwm_init34(7500, 0, 0);
+}
+
+void move(uint16_t leftDuty, uint16_t rightDuty)
+{
+    P3->OUT |= 0xC0;
+    TIMER_A0->CCR[3] = leftDuty;
+    TIMER_A0->CCR[4] = rightDuty;
+}
+
+void left_forward()
+{
+    P5->OUT &= ~0x10;
+}
+
+void left_backward()
+{
+    P5->OUT |= 0x10;
+}
+
+void right_forward()
+{
+    P5->OUT &= ~0x20;
+}
+
+void right_backward()
+{
+    P5->OUT |= 0x20;
+}
+
+// Timer A3
+void timer_A3_capture_init(void)
+{
+    P10->SEL0 |= 0x30;
+    P10->SEL1 &= ~0x30;
+    P10->DIR  &= ~0x30;
+
+    TIMER_A3->CTL &= ~0x0030;
+    TIMER_A3->CTL = 0x0200;
+
+    TIMER_A3->CCTL[0] = 0x4910;
+    TIMER_A3->CCTL[1] = 0x4910;
+    TIMER_A3->EX0 &= ~0x0007;
+
+    NVIC->IP[3] = (NVIC->IP[3]&0x0000FFFF) | 0x40400000;
+    NVIC->ISER[0] = 0x0000C000;
+    TIMER_A3->CTL |= 0x0024;
+}
+
+uint16_t first_left;
+uint16_t first_right;
+
+uint16_t period_left;
+uint16_t period_right;
+
+uint32_t right_count;
+
+void TA3_0_IRQHandler()
+{
+    TIMER_A3->CCTL[0] &= ~0x0001;
+    period_right = TIMER_A3->CCR[0] - first_right;
+    first_right = TIMER_A3->CCR[0];
+
+    right_count++;
+}
+
+uint32_t left_count;
+void TA3_N_IRQHandler(void)
+{
+    TIMER_A3->CCTL[1] &= ~0x0001;
+    period_left = TIMER_A3->CCR[1] - first_left;
+    first_left = TIMER_A3->CCR[1];
+
+
+    left_count++;
+}
+
+uint32_t get_left_rpm()
+{
+    return 2000000 / period_left;
+}
+
+//void TA3_N_IRQHandler()
+//{
+//    TIMER_A3->CCTL[1] &= ~0x0001;
+//    left_count++;
+//}
+
+
+
+int main(void)
+{
+    // Initialize
+    Clock_Init48MHz();
+    led_init();
+    IR_Init();
+    motor_init();
+    systick_init();
+    timer_A3_capture_init();
+
+    float lspeed = 1000;
+    float rspeed = 1000;
+    short isEnterBlackArea = 0;
+    short isEnterWhiteArea = 0;
+
+    float change = 430;
+
+    int dir = 0; // 0 : straight, 1 : clockwise, -1: counterclockwise
+
+    while(1)
+    {
+        P5->OUT |= 0x08;
+        P9->OUT |= 0x04;
+
+        P7->DIR = 0xFF;
+        P7->OUT = 0xFF;
+
+        Clock_Delay1us(10);
+
+        P7->DIR = 0x00;
+
+        Clock_Delay1us(1000);
+
+        right_forward();
+        left_forward();
+
+        if(isEnterWhiteArea) // In white space
+        {
+            if(P7->IN != 0b00000000) // stop
+            {
+                P2->OUT &= ~0xC0;
+
+                move(0,0);
+            }
+            else // go
+            {
+                move(lspeed, rspeed);
+                P2->OUT |=  0xC0;
+
+                systick_wait1ms();
+            }
+        }
+        else
+        {
+            // control both wheels speed
+            if(P7->IN == 0b11111111) // ignore
+            {
+                turn_on_led(LED_RED);
+                move(0, 0);
+            }
+            else
+            {
+                isEnterBlackArea = 1;
+
+                // Set direction
+                if(dir == 0)
+                {
+                    if((P7->IN & 0b00001100) == 0b00001100)
+                    {
+                        turn_on_led(LED_BLUE);
+
+                        dir = 1;
+
+                        rspeed += change;
+                        lspeed -= change;
+                    }
+                    else if((P7->IN & 0b00110000) == 0b00110000)
+                    {
+                        turn_on_led(LED_GREEN);
+
+                        dir = -1;
+
+                        rspeed -= change;
+                        lspeed += change;
+                    }
+                }
+
+                // Enter White Space
+                if(isEnterBlackArea && (P7->IN == 0b00000000))
+                {
+                    turn_on_led(LED_BLUE + LED_RED);
+
+                    isEnterWhiteArea = 1;
+                }
+
+                // move
+                move(lspeed, rspeed);
+                P2->OUT |=  0xC0;
+
+                systick_wait1ms();
+            }
+        }
+
+        P5->OUT &= ~0x08;
+        P9->OUT &= ~0x04;
+
+        Clock_Delay1ms(10);
+    }
+}
