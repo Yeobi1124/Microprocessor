55dd00d20e1d523ecddf7225b59e38df2a872d84
0x51018 <younhj1018@gmail.com>
2025-12-10 11:57:34 +0900

    stage2

--- LabProject/stage2.c
+++ LabProject/stage2.c
@@ -1,141 +1,51 @@
 #include "msp.h"
 #include "Clock.h"
+#include <stdint.h>
 #include <stdio.h>
 
-#define LED_OFF     0b000
-#define LED_RED     0b001
-#define LED_GREEN   0b010
-#define LED_BLUE    0b100
-#define LED_YELLOW  0b011
-#define LED_PURPLE  0b101
-#define LED_CYAN    0b110
+#define PWM_PERIOD 7500
+#define DEFAULT_SPEED 1
 
-void systick_init()
-{
-    SysTick->LOAD = 0x00FFFFFF;
-    SysTick->CTRL = 0x00000005;
+#define N_SLEEP_PINS (0x40 | 0x80)
+#define DIR_PINS     (0x10 | 0x20)
+#define PWM_PINS     (0x40 | 0x80)
+
+#define ENCODER_PINS (0x10 | 0x20)
+#define RIGHT_ENCODER_CHANNEL 0
+#define LEFT_ENCODER_CHANNEL 1
+
+#define COUNTS_FOR_TEST 10
+
+volatile uint32_t Right_Encoder_Count = 0;
+volatile uint32_t Left_Encoder_Count = 0;
+
+void move(uint16_t leftDuty, uint16_t rightDuty) { TIMER_A0->CCR[4] = leftDuty; TIMER_A0->CCR[3] = rightDuty; }
+void left_forward() { P5->OUT &= ~0x10; }
+void left_backward() { P5->OUT |= 0x10; }
+void right_forward() { P5->OUT &= ~0x20; }
+void right_backward() { P5->OUT |= 0x20; }
+void stop_motors() { P5->OUT &= 0; }
+void enable_motors() { P3->OUT |= N_SLEEP_PINS; }
+void disable_motors() { stop_motors(); P3->OUT &= ~N_SLEEP_PINS; }
+void motor_init(void) {
+    P3->SEL0 &= ~N_SLEEP_PINS; P3->SEL1 &= ~N_SLEEP_PINS; P3->DIR |= N_SLEEP_PINS; P3->OUT &= ~N_SLEEP_PINS;
+    P5->SEL0 &= ~DIR_PINS; P5->SEL1 &= ~DIR_PINS; P5->DIR |= DIR_PINS; P5->OUT &= ~DIR_PINS;
+    P2->SEL0 |= PWM_PINS; P2->SEL1 &= ~PWM_PINS; P2->DIR |= PWM_PINS;
+
+    TIMER_A0->CTL = 0x0280;
+    TIMER_A0->EX0 = 0x0001;
+    TIMER_A0->CCR[0] = PWM_PERIOD;
+    TIMER_A0->CCTL[3] = 0x00E0; TIMER_A0->CCR[3] = 0;
+    TIMER_A0->CCTL[4] = 0x00E0; TIMER_A0->CCR[4] = 0;
+    TIMER_A0->CTL |= 0x0030;
 }
 
-void systick_wait1ms()
-{
-    SysTick->LOAD = 48000; // 48000 Hz Clock Hardware?
-    SysTick->VAL = 0;
-    while((SysTick->CTRL & 0x00010000) == 0){};
-}
+volatile int save[10000],li=0;
 
-void systick_wait1s()
-{
-    int i = 0;
-    int count = 1000;
-    for(i=0;i<count;i++)
-    {
-        systick_wait1ms();
-    }
-}
-
-void led_init()
-{
-    P2->SEL0 &= ~0x07;
-    P2->SEL1 &= ~0x07;
-    P2->DIR |= 0x07;
-    P2->OUT &= ~0x07;
-}
-
-void turn_on_led(int color)
-{
-    P2->OUT = (P2->OUT & ~0x07) | (color & 0x07);
-}
-
-void IR_Init()
-{
-    // 0, 2, 4, 6 IR
-    P5->SEL0 &= ~0x08;
-    P5->SEL1 &= ~0x08;      // GPIO
-    P5->DIR  |=  0x08;      // OUTPUT
-    P5->OUT  &= ~0x08;      // turn off 4 even IR LEDs
-
-    // 1, 3, 5, 7 IR
-    P9->SEL0 &= ~0x04;
-    P9->SEL1 &= ~0x04;      // GPIO
-    P9->DIR  |=  0x04;      // OUTPUT
-    P9->OUT  &= ~0x04;      // turn off 4 odd IR LEDs
-
-    // 0~7 IR SEnsor
-    P7->SEL0 &= ~0xFF;
-    P7->SEL1 &= ~0xFF;      // GPIO
-    P7->DIR  &= ~0xFF;      // INPUT
-}
-
-void pwm_init34(uint16_t period, uint16_t duty3, uint16_t duty4)
-{
-    TIMER_A0->CCR[0] = period;
-
-    TIMER_A0->EX0 =0x0000;
-
-    TIMER_A0->CCTL[3] = 0x0040;
-    TIMER_A0->CCR[3] = duty3;
-    TIMER_A0->CCTL[4] = 0x0040;
-    TIMER_A0->CCR[4] = duty4;
-
-    TIMER_A0->CTL = 0x02F0;
-
-    P2->DIR |= 0xC0;
-    P2->SEL0 |= 0xC0;
-    P2->SEL1 &=~0xC0;
-}
-
-void motor_init()
-{
-    P3->SEL0 &= ~0xC0;
-    P3->SEL1 &= ~0xC0;
-    P3->DIR  |=  0xC0;
-    P3->OUT  &= ~0xC0;
-
-    P5->SEL0 &= ~0x30;
-    P5->SEL1 &= ~0x30;
-    P5->DIR  |=  0x30;
-    P5->OUT  &= ~0x30;
-
-    P2->SEL0 &= ~0xC0;
-    P2->SEL1 &= ~0xC0;
-    P2->DIR  |=  0xC0;
-    P2->OUT  &= ~0xC0;
-
-    pwm_init34(7500, 0, 0);
-}
-
-void move(uint16_t leftDuty, uint16_t rightDuty)
-{
-    P3->OUT |= 0xC0;
-    TIMER_A0->CCR[3] = leftDuty;
-    TIMER_A0->CCR[4] = rightDuty;
-}
-
-void left_forward()
-{
-    P5->OUT &= ~0x10;
-}
-
-void left_backward()
-{
-    P5->OUT |= 0x10;
-}
-
-void right_forward()
-{
-    P5->OUT &= ~0x20;
-}
-
-void right_backward()
-{
-    P5->OUT |= 0x20;
-}
-
-void timer_A3_capture_init(void)
-{
+void tachometer_init(void) {
     P10->SEL0 |= 0x30;
     P10->SEL1 &= ~0x30;
-    P10->DIR  &= ~0x30;
+    P10->DIR &= ~0x30;
 
     TIMER_A3->CTL &= ~0x0030;
     TIMER_A3->CTL = 0x0200;
@@ -144,228 +54,132 @@ void timer_A3_capture_init(void)
     TIMER_A3->CCTL[1] = 0x4910;
     TIMER_A3->EX0 &= ~0x0007;
 
-    NVIC->IP[3] = (NVIC->IP[3]&0x0000FFFF) | 0x40400000;
-    NVIC->ISER[0] = 0x0000C000;
+    NVIC->IP[3]=(NVIC->IP[3]&0x0000FFFF)|0x404000000;
+    NVIC->ISER[0]=0x0000C000;
+
     TIMER_A3->CTL |= 0x0024;
 }
-
-uint16_t first_left;
-uint16_t first_right;
-
-uint16_t period_left;
-uint16_t period_right;
-
-uint32_t right_count;
-uint32_t left_count;
-
-void TA3_0_IRQHandler()
-{
-    TIMER_A3->CCTL[0] &= ~0x0001;
-    period_right = TIMER_A3->CCR[0] - first_right;
-    first_right = TIMER_A3->CCR[0];
-
-    right_count++;
-    P2->OUT &= ~0x07;
-    P2->OUT |= 0x02;
+void sensor_init(void){
+    P5->SEL0&=~0x08;
+    P5->SEL1&=~0x08;
+    P5->DIR|=0x08;
+    P5->OUT&=~0x08;
+    P9->SEL0&=~0x04;
+    P9->SEL1&=~0x04;
+    P9->DIR|=0x04;
+    P9->OUT&=~0x04;
+    P7->SEL0&=~0xFF;
+    P7->SEL1&=~0xFF;
+    P7->DIR&=~0xFF;
 }
 
-void TA3_N_IRQHandler(void)
-{
-    TIMER_A3->CCTL[1] &= ~0x0001;
-    period_left = TIMER_A3->CCR[1] - first_left;
-    first_left = TIMER_A3->CCR[1];
-
-    left_count++;
-    P2->OUT &= ~0x07;
-    P2->OUT |= 0x01;
+void TA3_0_IRQHandler(void) {
+    TIMER_A3->CCTL[RIGHT_ENCODER_CHANNEL] &= ~0x0001;
+    Right_Encoder_Count++;
 }
 
-uint32_t get_left_rpm()
-{
-    return 2000000 / period_left;
+void TA3_N_IRQHandler(void) {
+    TIMER_A3->CCTL[LEFT_ENCODER_CHANNEL] &= ~0x0001;
+    Left_Encoder_Count++;
 }
 
-uint32_t get_right_rpm()
-{
-    return 2000000 / period_right;
-}
-
-void rotate_right(uint16_t duty)
-{
-    right_backward();
-    left_forward();
-    move(duty, duty);
-}
-
-void rotate_left(uint16_t duty)
-{
-    right_forward();
-    left_backward();
-    move(duty, duty);
-}
-
-int main(void)
-{
-    // Initialize
-    Clock_Init48MHz();
-    Clock_Delay1ms(100);
-    led_init();
-    IR_Init();
-    motor_init();
-    systick_init();
-    timer_A3_capture_init();
-
-    int sensor;
-    int step = 1;
-    int cnt = 0;
-    int left_speed = 800, right_speed = 800;
-    int stage_progress = 0;
-    // step 1: 직진하는 상태
-    // step 2: 왼쪽으로 회전해야 하는 상태
-    // step 3: 오른쪽으로 회전해야 하는 상태
-    // step 4: 곡선 추적 상태
-    // step 5: 종료 상태
-    while(1)
-    {   
-        P5->OUT |= 0x08;
-        P9->OUT |= 0x04;
-
-        P7->DIR = 0xFF;
-        P7->OUT = 0xFF;
-
-        Clock_Delay1us(10);
-
-        P7->DIR = 0x00;
-
-        Clock_Delay1us(10);
-
-        if(step == 0){
-            move(0,0);
-            if((P7->IN & 0b00011000) == 0b00011000){
-                step = 1;
-            }
-        }
-        else if(step == 1)
-        {   
-            turn_on_led(LED_GREEN);
-            if((P7->IN & 0b00011000) == 0b00011000){
-                // align 잘 된 경우
-                right_forward();
-                left_forward();
-                move(left_speed, right_speed);
-                P2->OUT |=  0xC0;
-                systick_wait1ms();
-            }
-            else{
-                if(cnt == 0 || cnt == 3 || cnt == 4) step = 2;
-                else step = 3;
-                cnt++;
-                left_count = 0; right_count = 0;
-            }
-        }
-        else if(step == 2)
-        {
-            turn_on_led(LED_BLUE);
-            rotate_left(4000);
-
-            if(right_count > 1500){
-                step = 1;
-                if (cnt == 4) step = 4;
-                left_count = 0;
-                right_count = 0;
-            }
-        }
-        else if(step == 3)
-        {
-            turn_on_led(LED_BLUE);
-            rotate_right(4000);
-
-            if(left_count > 1500){
-                step = 1;
-                if (cnt == 5) step = 5;
-                left_count = 0;
-                right_count = 0;
-            }
-        }
-        else if(step == 4)
-        {
-            turn_on_led(LED_YELLOW);
-            if((P7->IN & 0b00011000) == 0b00011000){
-                // align 잘 된 경우
-                right_forward();
-                left_forward();
-                move(left_speed, right_speed);
-                P2->OUT |=  0xC0;
-                systick_wait1ms();
-            }
-            else if((P7->IN & 0b00011000) == 0b0000000){
-                step = 3;
-                cnt++;
-            }
-            else{
-                if((P7->IN & 0b00010000) == 0){ 
-                    // 선에서 왼쪽으로 벗어난 경우
-                    right_backward(); left_forward();
-                    move(200, 200);
-                    P2->OUT &= ~0x07;
-                    P2->OUT |= 0x04;
-                    systick_wait1ms();
-                }
-                else if((P7->IN & 0b00001000) == 0){ 
-                    // 선에서 오른쪽으로 벗어난 경우
-                    right_forward(); left_backward();
-                    move(200, 200);
-                    P2->OUT &= ~0x07;
-                    P2->OUT |= 0x04;
-                    systick_wait1ms();
-                }
-            }
-        }
-        else if(step == 5)
-        {
-            turn_on_led(LED_PURPLE);
-            if((P7->IN & 0b00011000) == 0b00011000){
-                // align 잘 된 경우
-                right_forward();
-                left_forward();
-                move(left_speed, right_speed);
-                P2->OUT |=  0xC0;
-                systick_wait1ms();
-            }
-            else if((P7->IN & 0b01111110) == 0b01111110){
-                // 종료 선 만난 경우
-                P2->OUT &= ~0x07;
-                P2->OUT |=  0x02;
-
-                move(0,0);
-            }
-        }
-        else{
-            turn_on_led(LED_CYAN);
-            move(0,0);
-        }
-
-
-        
-        // else if((P7->IN & 0b01111110) == 0b01111110){
-        //     // 종료 선 만난 경우
-        //     P2->OUT &= ~0x07;
-        //     P2->OUT |=  0x02;
-
-        //     move(0,0);
-        // }
-
-        P5->OUT &= ~0x08;
-        P9->OUT &= ~0x04;
-
-        Clock_Delay1ms(10);
-
-//        P5->OUT &= ~0x30;
-//        P2->OUT |=  0xC0;
-//        P3->OUT |=  0xC0;
-//        Clock_Delay1ms(1000);
-//
-//        P2->OUT &= ~0xC0;
-//        Clock_Delay1ms(1000);
+uint32_t state(void){
+    P5->OUT|=0x08;
+    P9->OUT|=0x04;
+    P7->DIR=0xFF;
+    P7->OUT=0xFF;
+    Clock_Delay1us(10);
+    P7->DIR=0x00;
+    Clock_Delay1us(1500);
+    int result=P7->IN,temp=result,cnt=0;
+    printf("%d\n",result);
+    while(temp>0){
+        if(temp&1)
+            cnt++;
+        temp/=2;
     }
+    if(cnt>7)
+        return 0;
+    int cr=(result/4)%16;
+    int l=cr%4,r=(cr/4%2)*2+(cr/8);
+    if(l==0&&r==0)
+        return 0;
+    if(l==r)
+        return 1;
+    if(l<r)
+        return 2;
+    if(l>r)
+        return 3;
+    return 1;
+}
+
+uint32_t state1(void){
+    P5->OUT|=0x08;
+    P9->OUT|=0x04;
+    P7->DIR=0xFF;
+    P7->OUT=0xFF;
+    Clock_Delay1us(10);
+    P7->DIR=0x00;
+    Clock_Delay1us(1500);
+    int result=P7->IN,temp=result,cnt=0;
+    printf("%d\n",result);
+    while(temp>0){
+        if(temp&1) cnt++;
+        temp/=2;
+    }
+    if(cnt>7)
+        return 0;
+    int cr=(result/4)%16;
+    int l=cr%4,r=(cr/4%2)*2+(cr/8);
+    if(l==0&&r==0) return 1;
+    if(l==r) return 1;
+    if(l<r) return 2;
+    if(l>r) return 3;
+    return 0;
+}
+
+void move1(void){
+    move(DEFAULT_SPEED, DEFAULT_SPEED);
+    volatile uint32_t action=state();
+    while(action>0){
+        printf("%d\n",action);
+        if(action==0){
+            stop_motors();
+        }
+        if(action==1){
+            //stop_motors();
+            left_forward();
+            right_forward();
+        }
+        else if(action==2){
+            //stop_motors();
+            left_backward();
+            right_forward();
+        }
+        else if(action==3){
+            //stop_motors();
+            right_backward();
+            right_backward();
+        }
+        enable_motors();
+        Clock_Delay1us(750);
+        disable_motors();
+        Clock_Delay1ms(1);
+        action=state();
+    }
+    stop_motors();
+    disable_motors();
+    Clock_Delay1ms(500);
+}
+
+void main(void) {
+    Clock_Init48MHz();
+    sensor_init();
+    motor_init();
+    tachometer_init();
+    __enable_irq();
+    disable_motors();
+    Clock_Delay1ms(1000);
+    move1();
+    return ;
 }
